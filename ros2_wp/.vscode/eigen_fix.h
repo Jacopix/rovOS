76bd0228b89376ace0ec53b6219f7111931cb86c
Jacopo <68518128+Jacopix@users.noreply.github.com>
2025-03-31 01:32:15 +0200

    Added other configuration files for IntelliSense (to be checked)

--- ros2_wp/.vscode/eigen_fix.h
+++ ros2_wp/.vscode/eigen_fix.h
@@ -2,3 +2,4 @@
 #undef __ARM_NEON
 #undef __ARM_NEON__
 #endif
+// Fix from https://github.com/microsoft/vscode-cpptools/issues/7413#:~:text=containing%20the%20fix-,%23if%20__INTELLISENSE__%0A%23undef%20__ARM_NEON%0A%23undef%20__ARM_NEON__%0A%23endif,-2.%20Add%20it
